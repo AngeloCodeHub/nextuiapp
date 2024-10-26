8fedf92ac643d0a2cec4fec37828e0e0d61391b3
Angelo <a0920217700@gmail.com>
2024-10-26 16:22:04 +0800

    chapter 4 finished

--- app/ui/acme-logo.tsx
+++ app/ui/acme-logo.tsx
@@ -6,7 +6,7 @@ export default function AcmeLogo() {
     <div className={`${lusitana.className} flex flex-row items-center leading-none text-white`} >
       <GlobeAltIcon className="h-12 w-12 rotate-[15deg]" />
       {/* <p className="text-[44px]">北半球</p> */}
-      <strong className="text-[40px]">北半球</strong>
+      <strong className="text-[32px]">北半球</strong>
     </div>
   );
 }
