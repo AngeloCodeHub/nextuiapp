f6d5c07f12bf34379b930571df0d9bd0e500de50
Angelo <a0920217700@gmail.com>
2024-10-26 14:53:15 +0800

    dashboardApp初始化 Chapter 1～3 finished

--- config/site.ts
+++ config/site.ts
@@ -1,7 +1,7 @@
 export type SiteConfig = typeof siteConfig;
 
 export const siteConfig = {
-  name: "Next.js + NextUI",
+  name: "儀表板App",
   description: "Make beautiful websites regardless of your design experience.",
   navItems: [
     {
