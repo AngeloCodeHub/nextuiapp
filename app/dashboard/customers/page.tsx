d26e53a4f4c10e18dd5effa78f912b78fd420ff0
Angelo <a0920217700@gmail.com>
2024-10-26 16:22:04 +0800

    chapter 4 finished

---
+++ app/dashboard/customers/page.tsx
@@ -0,0 +1,8 @@
+
+
+export default function Page() {
+    return (
+
+        <p className="text-2xl italic">客戶頁面</p>
+    )
+}
---
+++ app/dashboard/page.tsx
@@ -0,0 +1,8 @@
+
+
+
+export default function Page() {
+    return (
+        <p>儀表板頁面</p>
+    )
+}
