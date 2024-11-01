3f910ca6eb71d0c910a94e9bb7f71512f298a2bf
Angelo <a0920217700@gmail.com>
2024-11-01 12:53:06 +0800

    Chapter 13~14

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -6,44 +6,93 @@ import { sql } from "@vercel/postgres";
 import { revalidatePath } from "next/cache";
 import { redirect } from "next/navigation";
 
-const formSchema = z.object({
+const FormSchema = z.object({
     id: z.string(),
-    customerId: z.string(),
-    amount: z.coerce.number(),
-    status: z.enum(["paid", "pending"]),
+    customerId: z.string({
+        invalid_type_error: '請選擇客戶'
+    }),
+    amount: z.coerce.number()
+        .gt(0, { message: '請輸入大於 0 的金額' })
+    ,
+    status: z.enum(["paid", "pending"], {
+        invalid_type_error: '請選擇付款狀態'
+    }),
     date: z.string()
 });
 
-const UpdateInvoice = formSchema.omit({ id: true, date: true });
-const CreateInvoice = formSchema.omit({ id: true, date: true });
+const UpdateInvoice = FormSchema.omit({ id: true, date: true });
+const CreateInvoice = FormSchema.omit({ id: true, date: true });
 
+export type State = {
+    errors?: {
+        customerId?: string[];
+        amount?: string[];
+        status?: string[];
+    };
+    message?: string | null;
+}
 
-export async function updateInvoice(id: string, formData: FormData) {
-    const { customerId, amount, status } = UpdateInvoice.parse({
+export async function updateInvoice(id: string, prevState: State, formData: FormData) {
+
+    const validatedFields = UpdateInvoice.safeParse({
         customerId: formData.get('customerId'),
         amount: formData.get('amount'),
         status: formData.get('status'),
     });
 
+    if (!validatedFields.success) {
+        return {
+            errors: validatedFields.error.flatten().fieldErrors,
+            message: 'Missing Fields. Failed to Update Invoice.',
+        };
+    }
+    // const { customerId, amount, status } = UpdateInvoice.parse({
+    //     customerId: formData.get('customerId'),
+    //     amount: formData.get('amount'),
+    //     status: formData.get('status'),
+    // });
+    const { customerId, amount, status } = validatedFields.data;
     const amountInCents = amount * 100;
 
-    await sql`
-      UPDATE invoices
-      SET customer_id = ${customerId}, amount = ${amountInCents}, status = ${status}
-      WHERE id = ${id}
-    `;
+    try {
+        await sql`
+          UPDATE invoices
+          SET customer_id = ${customerId}, amount = ${amountInCents}, status = ${status}
+          WHERE id = ${id}
+        `;
+    } catch (error) {
+        return { message: '資料庫錯誤：Failed to update invoice.' };
+    }
 
     revalidatePath('/dashboard/invoices');
     redirect('/dashboard/invoices');
 }
 
 export async function deleteInvoice(id: string) {
-    await sql`DELETE FROM invoices WHERE id = ${id}`;
-    revalidatePath('/dashboard/invoices');
+    // throw new Error('Failed to Delete Invoice');
+    try {
+        await sql`DELETE FROM invoices WHERE id = ${id}`;
+        revalidatePath('/dashboard/invoices');
+        return { message: 'Invoice deleted successfully.' };
+    } catch (error) {
+        return { message: '資料庫錯誤：Failed to delete invoice.' };
+    }
 }
 
+export async function createInvoice(prevState: State, formData: FormData) {
 
-export async function createInvoice(formData: FormData) {
+    const validatedFields = CreateInvoice.safeParse({
+        customerId: formData.get('customerId'),
+        amount: formData.get('amount'),
+        status: formData.get('status')
+    });
+
+    if (!validatedFields.success) {
+        return {
+            errors: validatedFields.error.flatten().fieldErrors,
+            message: '請修正表單錯誤'
+        }
+    }
 
     // const rawFormData = {
     //     customerId: formData.get('customerId'),
@@ -52,19 +101,19 @@ export async function createInvoice(formData: FormData) {
     // };
     // console.log(rawFormData);
 
-    const { customerId, amount, status } = CreateInvoice.parse({
-        customerId: formData.get('customerId'),
-        amount: formData.get('amount'),
-        status: formData.get('status')
-    });
-
+    const { customerId, amount, status } = validatedFields.data;
     const amountInCents = amount * 100;
     const date = new Date().toISOString().split('T')[0];
 
-    await sql`
-    INSERT INTO invoices (customer_id, amount, status, date)
-    VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
-    `;
+    try {
+        await sql`
+        INSERT INTO invoices (customer_id, amount, status, date)
+        VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
+        `;
+
+    } catch (error) {
+        return { message: '資料庫錯誤：Failed to create invoice.' };
+    }
 
     revalidatePath('/dashboard/invoices');
     redirect('/dashboard/invoices');
